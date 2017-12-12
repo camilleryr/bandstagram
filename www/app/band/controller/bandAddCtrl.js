a249b2615646fceaf895e664edc822d641c602f8
Christopher Miller <camiller.yr@gmail.com>
2017-12-12 14:30:09 -0600

    working on recording functions

--- www/app/band/controller/bandAddCtrl.js
+++ www/app/band/controller/bandAddCtrl.js
@@ -1,39 +1,58 @@
 angular.module('bandstagram')
-.controller('bandAddCtrl', function($scope, $state, mediaFactory, $cordovaCamera, $ionicModal) {
-    $scope.srcImage = "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fcnet4.cbsistatic.com%2Ffly%2F893-fly%2Fbundles%2Fcnetcss%2Fimages%2Fplaceholder%2Fimage_placeholder.png&f=1"
-    $scope.recordingInfo = {}
+    .controller('bandAddCtrl', function ($scope, $state, mediaFactory, $cordovaCamera, $cordovaMedia, $ionicModal) {
+        $scope.srcImage = "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fcnet4.cbsistatic.com%2Ffly%2F893-fly%2Fbundles%2Fcnetcss%2Fimages%2Fplaceholder%2Fimage_placeholder.png&f=1"
+        $scope.recordingInfo = {}
 
-    $scope.takePicture = function(){
+        $scope.takePicture = function () {
 
-        $cordovaCamera.getPicture(mediaFactory.photoOptions).then(function(imageData) {
-            $scope.srcImage = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            // error
-        });
-    }
-
-    $ionicModal.fromTemplateUrl('app/auth/partial/modal.html', {
-        scope: $scope,
-        animation: 'slide-in-left'
-    }).then(function (modal) {
-        $scope.modal = modal;
-    });
-
-    $scope.openModal = function () {
-        $scope.modal.show();
-    };
-
-    $scope.closeModal = function (bool) {
-        $scope.modal.hide();
-        if (bool) {
-            authFactory.registerWithEmail($scope.auth)
-                .then(function (user) {
-                    user.updateProfile({ "displayName": $scope.auth.account })
-                    $scope.userInfo.uid = user.uid
-                    databaseFactory.postUserInfo($scope.auth.account, $scope.userInfo)
-                    $scope.login = {}
-                })
+            $cordovaCamera.getPicture(mediaFactory.photoOptions).then(function (imageData) {
+                $scope.srcImage = "data:image/jpeg;base64," + imageData;
+            }, function (err) {
+                // error
+            });
         }
-    };
-    
-})
+
+        $ionicModal.fromTemplateUrl('app/band/partial/addRecordingModal.html', {
+            scope: $scope,
+            animation: 'slide-in-left'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openModal = function () {
+            $scope.modal.show();
+        };
+
+        $scope.closeModal = function (bool) {
+            $scope.modal.hide();
+
+        }
+
+        $scope.recording = function(){
+            let recordingObj
+            let name
+            return Object.create(null, {
+                "start" : {
+                    "value" : function () {
+                        console.log("test")
+                        name = "cdvfile://localhost/temporary/" + Date.now() + ".m4a"
+                        recordingObj = $cordovaMedia.newMedia(name)
+
+                        recordingObj.startRecord();
+                    }
+                },
+                "stop" : {
+                    "value" : function () {
+                        recordingObj.startRecord();
+                    }
+                },
+                "play" : {
+                    "value" : function () {
+                        recordingObj.play();
+                    }
+                },
+            })
+        }
+
+
+    })
--- www/app/util/mediaFactory.js
+++ www/app/util/mediaFactory.js
@@ -21,84 +21,84 @@ angular.module('bandstagram')
           }
       },
 
-      "savePicture": {
-        "value": (imageData) => {
+    //   "savePicture": {
+    //     "value": (imageData) => {
 
-          let fileRef = storageRef.child("images/" + currentUserData.uid + Date.now() + ".jpeg");
-          let uploadTask = fileRef.putString(imageData, 'data_url')
-        }
-      },
+    //       let fileRef = storageRef.child("images/" + currentUserData.uid + Date.now() + ".jpeg");
+    //       let uploadTask = fileRef.putString(imageData, 'data_url')
+    //     }
+    //   },
 
-      // "startRecording": {
-      //   "value": () => {
-      //     let name = "cdvfile://localhost/temporary/" + Date.now() + ".m4a"
-      //     let newRecording = $cordovaMedia.newMedia(name)
+    //   "startRecording": {
+    //     "value": () => {
+    //       let name = "cdvfile://localhost/temporary/" + Date.now() + ".m4a"
+    //       let newRecording = $cordovaMedia.newMedia(name)
 
-      //     // Record audio
-      //     mediaRec.startRecord();
-      //   }
-      // },
+    //       // Record audio
+    //       mediaRec.startRecord();
+    //     }
+    //   },
 
-      // "stopRecording": {
-      //   "value": mediaObject => mediaObject.stopRecord()
-      // },
+    //   "stopRecording": {
+    //     "value": mediaObject => mediaObject.stopRecord()
+    //   },
 
-      // "playAudio": {
-      //   "value": {}
-      // },
+    //   "playAudio": {
+    //     "value": {}
+    //   },
 
-      // "stopAudio": {
-      //   "value": {}
-      // },
+    //   "stopAudio": {
+    //     "value": {}
+    //   },
 
-      // "saveRecording": {
-      //   "value": {}
-      // },
+    //   "saveRecording": {
+    //     "value": {}
+    //   // },
 
-      "uploadWatch": {
-        "value": () => {
-          // Listen for state changes, errors, and completion of the upload.
-          uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-            function (snapshot) {
-              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log('Upload is ' + progress + '% done');
-              switch (snapshot.state) {
-                case firebase.storage.TaskState.PAUSED: // or 'paused'
-                  console.log('Upload is paused');
-                  break;
-                case firebase.storage.TaskState.RUNNING: // or 'running'
-                  console.log('Upload is running');
-                  break;
-              }
-            }, function (error) {
+    //   "uploadWatch": {
+    //     "value": () => {
+    //       // Listen for state changes, errors, and completion of the upload.
+    //       uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    //         function (snapshot) {
+    //           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    //           var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //           console.log('Upload is ' + progress + '% done');
+    //           switch (snapshot.state) {
+    //             case firebase.storage.TaskState.PAUSED: // or 'paused'
+    //               console.log('Upload is paused');
+    //               break;
+    //             case firebase.storage.TaskState.RUNNING: // or 'running'
+    //               console.log('Upload is running');
+    //               break;
+    //           }
+    //         }, function (error) {
 
-              // A full list of error codes is available at
-              // https://firebase.google.com/docs/storage/web/handle-errors
-              switch (error.code) {
-                case 'storage/unauthorized':
-                  // User doesn't have permission to access the object
-                  break;
+    //           // A full list of error codes is available at
+    //           // https://firebase.google.com/docs/storage/web/handle-errors
+    //           switch (error.code) {
+    //             case 'storage/unauthorized':
+    //               // User doesn't have permission to access the object
+    //               break;
 
-                case 'storage/canceled':
-                  // User canceled the upload
-                  break;
+    //             case 'storage/canceled':
+    //               // User canceled the upload
+    //               break;
 
-                case 'storage/unknown':
-                  // Unknown error occurred, inspect error.serverResponse
-                  break;
-              }
-            }, function () {
-              // Upload completed successfully, now we can get the download URL
-              var downloadURL = uploadTask.snapshot.downloadURL;
-              console.log(downloadURL)
-              // })
-            });
+    //             case 'storage/unknown':
+    //               // Unknown error occurred, inspect error.serverResponse
+    //               break;
+    //           }
+    //         }, function () {
+    //           // Upload completed successfully, now we can get the download URL
+    //           var downloadURL = uploadTask.snapshot.downloadURL;
+    //           console.log(downloadURL)
+    //           // })
+    //         });
 
-        }, function(err) {
-          // error
-        }
-      }
+    //     }, function(err) {
+    //       // error
+    //     }
+    //   }
     })
   })
 
