a23d92cadb507a0b731f69588a97fba4211539f1
Christopher Miller <camiller.yr@gmail.com>
2017-12-28 12:37:37 -0600

    Changed some stying to make it all dark and changed the add recording button to be 'cooler'

--- www/app/auth/controller/authCtrl.js
+++ www/app/auth/controller/authCtrl.js
@@ -1,23 +1,11 @@
 
 
 angular.module('bandstagram')
-    .controller('authCtrl', function ($scope, $state, databaseFactory, authFactory, photoFactory, $ionicModal, $firebaseAuth, $cordovaOauth, facebookFactory) {
-
-        var fb = firebase.database().ref()
-
-        $scope.placeholder = photoFactory.placeholder
-
-        $scope.takePicture = function () {
-            photoFactory.takePhoto().then(result => {
-                console.log(result)
-                $scope.userInfo.photoURL = result
-            })
-        }
+    .controller('authCtrl', function ($scope, authFactory) {
 
         $scope.auth = {
             "email": null,
-            "password": null,
-            "account": null,
+            "password": null
         }
         
         $scope.logMeIn = function () {
@@ -34,68 +22,7 @@ angular.module('bandstagram')
         }
         
         $scope.signupFacebook = function() {
-            
-            // var provider = new firebase.auth.FacebookAuthProvider();
-            if(window.cordova){
-
-                $cordovaOauth.facebook("172907863311544", ["email", "user_likes"]).then(function(result) {
-                    console.log(JSON.stringify(result))
-
-                    facebookFactory.setFbToken(result.access_token)
-                    
-                    var credential = firebase.auth.FacebookAuthProvider.credential(result.access_token)
-                    
-                    firebase.auth().signInWithCredential(credential).catch(function(error) {console.log(JSON.stringify(error))})
-                    
-                }, function(error) {
-                    console.log("ERROR: " + error);
-                });
-            } else {
-                
-                var provider = new firebase.auth.FacebookAuthProvider();
-                provider.addScope('user_likes')
-                
-                firebase.auth().signInWithPopup(provider).then(function(result) {
-                    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                    facebookFactory.setFbToken(result.credential.accessToken)
-
-                  }).catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // The email of the user's account used.
-                    var email = error.email;
-                    // The firebase.auth.AuthCredential type that was used.
-                    var credential = error.credential;
-                    // ...
-                  });
-            }
+            authFactory.loginWithFacebook()
         }
-        
-        
-        // $ionicModal.fromTemplateUrl('app/auth/partial/modal.html', {
-        //     scope: $scope,
-        //     animation: 'slide-in-up'
-        // }).then(function (modal) {
-        //     $scope.modal = modal;
-        // });
-        
-        // $scope.openModal = function () {
-        //     $scope.userInfo = {}
-        //     $scope.modal.show();
-        // };
-
-        // $scope.closeModal = function (bool) {
-        //     $scope.modal.hide();
-        //     if (bool) {
-        //         console.log("save")
-        //         $scope.user.updateProfile({ "displayName": $scope.auth.account })
-        //         $scope.userInfo.uid = $scope.user.uid
-        //         console.log($scope.userInfo)
-        //         databaseFactory.postUserInfo($scope.auth.account, $scope.userInfo)
-        //         authFactory.logout()
-        //         $scope.logMeIn()
-        //     }
-        // }
     })
 
